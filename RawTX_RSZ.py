d76166d0e673a496f4670847028614ebb525bb6a
TM <38635583+tm31420@users.noreply.github.com>
2021-07-04 21:02:52 -0400

    Add files via upload

---
+++ RawTX_RSZ.py
@@ -0,0 +1,37 @@
+import bitcoin
+import hashlib
+import txnUtils
+import keyUtils
+
+print('Enter your transaction:')
+tx = raw_input()
+m = txnUtils.parseTxn(tx)
+e = txnUtils.getSignableTxn(m)
+z = hashlib.sha256(hashlib.sha256(e.decode('hex')).digest()).digest()
+z1 = z[::-1].encode('hex_codec')
+z = z.encode('hex_codec')
+s = keyUtils.derSigToHexSig(m[1][:-2])
+pub =  m[2]
+sigR = s[:64]
+sigS = s[-64:]
+sigZ = z
+print ('Signed TX is :', tx)
+print ('Signature (r, s pair) is :', s)
+print ('Public Key is :', pub)
+print ("")
+print ("#################################################################################################")
+print ("")
+print ('Unsigned TX is :', e)
+print ('hash of message (sigZ) is USE This ONE :', z)
+print ('reversed z :', z1)
+print ("")
+print ("#################################################################################################")
+print ("##################################VALUES NEEDED ARE BELOW #######################################")
+print ("#################################################################################################")
+print ("")
+print ('THE R VALUE is  :', sigR)
+print ('THE S VALUE is  :', sigS)
+print ('THE Z VALUE is  :', sigZ)
+print ('THE PUBKEY is :', pub)
+
+
